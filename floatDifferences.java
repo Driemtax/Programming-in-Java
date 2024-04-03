21bddcf02a08644f833f10b3230872e412a9ab82
Lars <beer.lars@yahoo.de>
2024-04-03 21:58:43 +0200

    First Commit

---
+++ floatDifferences.java
@@ -0,0 +1,9 @@
+public class floatDifferences{
+    public static void main(String[] args){
+        double num1 = 0.1d;
+        double num2 = 0.2d;
+        double result = num1 + num2;
+
+        System.out.printf("%.15f", result);
+    }
+}
