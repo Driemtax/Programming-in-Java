1ab7924ea97942c955131067b7c36605fba74931
Lars Beer <lars.beer@stud.uni-frankfurt.de>
2024-04-05 17:32:20 +0200

    Small fixes

--- BinaryData.java
+++ BinaryData.java
@@ -6,8 +6,15 @@ public class BinaryData {
     }
 
     public byte ToBinary(int number){
-        byte result = 0b00001000;
+        byte result = 0b01111111;
 
+        /* while(result >= 1){
+            if (number % 2 == 1) {
+                result += 1;
+            }
+        } */
+
+        result += 1;
         return result;
-    }
+    } // rekursiv gestalten
 }
