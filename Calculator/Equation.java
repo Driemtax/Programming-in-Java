03c9f5569424e38694da464a897606446f810f24
Lars Beer <driemtax@gmail.com>
2024-04-12 17:50:48 +0200

    changes

--- Calculator/Equation.java
+++ Calculator/Equation.java
@@ -1,22 +1,27 @@
 public class Equation {
-    public double num1;
-    public double num2;
-    public char operator;
-    public double result;
-    public String errorMessage;
+    public static double Calculation(String expression){
+        double result = 0;
+        char lastOperator = '\0';
+        int operatorPosition;
+        double currentNumber = 0;
 
-    public Equation(String expression){
-        
         for (int i = 0; i < expression.length(); i++) {
             switch (expression.charAt(i)) {
                 case '+':
-                    num1 = Double.parseDouble(expression.substring(0, i));
-                    num2 = Double.parseDouble(expression.substring(i+1, expression.length()));
-                    result = Arithmetics.Addition(num1, num2);
+                    
+                    break;
+                case '-':
+                    break;
+                case '*':
+                    break;
+                case '/':
+                    break;
+                case '%':
                     break;
                 default:
                     break;
             }
         }
+        return result;
     }
 }
