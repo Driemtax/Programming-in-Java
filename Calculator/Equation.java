08aa9de320ee4fafcdbab961f05dab71c4a027b1
Lars Beer <driemtax@gmail.com>
2024-04-19 17:20:19 +0200

    Parser and Calculator partial

--- Calculator/Equation.java
+++ Calculator/Equation.java
@@ -1,27 +1,51 @@
 public class Equation {
-    public static double Calculation(String expression){
+    public static String Parser(String expression){ //expression = 3 * (4 + (24*3)) 
+        //Über Funktion gehen und nach Operatoren suchen
+        if (!(expression.contains("("))) {
+            expression = expression.concat(Double.toString(Calculate(expression)));
+            return expression;
+        }
+        int startBracket = 0;
+        for (int i = 0; i < expression.length(); i++) {
+            switch (expression.charAt(i)) {
+                case '(':
+                    startBracket = i+1;
+                    break;
+                case ')':
+                    expression = Parser(expression.substring(startBracket, i)); 
+                                                                        //expression2 = 4 + (24*3)
+                                                                        // expression3 = 24*3 = 72
+                                                                        // expression2 = 4 + 72 = 76
+                                                                        //expression1 = 3 * 76 = 228
+                    break;
+            }
+        }
+        return result;
+    }
+
+    public static double Calculate(String expression){
         double result = 0;
+        int indexLastOperator = 0;
         char lastOperator = '\0';
-        int operatorPosition;
-        double currentNumber = 0;
 
         for (int i = 0; i < expression.length(); i++) {
             switch (expression.charAt(i)) {
-                case '+':
-                    
-                    break;
-                case '-':
-                    break;
                 case '*':
                     break;
                 case '/':
                     break;
-                case '%':
+                case '+':
+                    if (lastOperator == '\0') {
+                        lastOperator = '+';
+                        indexLastOperator = i;
+                        break;
+                    }
                     break;
-                default:
+                case '-':
                     break;
             }
         }
+
         return result;
     }
 }
