63e542e11321ff542353949def4f55e562c658de
Lars Beer <driemtax@gmail.com>
2024-05-24 16:29:08 +0200

    small changes

--- Exercises/Ex6/Programm.java
+++ Exercises/Ex6/Programm.java
@@ -2,8 +2,10 @@ package Ex6;
 
 public class Programm {
     public static void main(String[] args) {
-        Airplane plane1 = new Airplane("LH 319", "Lissabon", "15:30");
+        Airplane plane1 = new Airplane("LH319", "Lissabon", "16:30");
+        Airplane plane2 = new Airplane("MH370", "Singapore", "14:00");
 
         plane1.checkStatus();
+        plane2.checkStatus();
     }
 }
