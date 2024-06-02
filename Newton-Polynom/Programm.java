61462a33d40028dac17854b618922b6fc8c0c568
Lars <driemtax@gmail.com>
2024-06-02 22:35:06 +0200

    Minor changes, ClassNotFoundException on Desktop

--- Newton-Polynom/Programm.java
+++ Newton-Polynom/Programm.java
@@ -13,8 +13,8 @@ public class Programm {
 
         System.out.printf("f(x)= %s\n", polynom);
 
-        int[] test = RationalPresentation.Fraction(4.5);
+        int[] test = RationalPresentation.Fraction(-4.5);
 
-        System.out.printf("Fraction of %f: %d/%d", 4.5, test[0], test[1]);
+        System.out.printf("Fraction of %f: %d/%d", -4.5, test[0], test[1]);
     }
 }
--- Test.java
+++ Test.java
@@ -1,8 +1,5 @@
 public class Test{
     public static void main(String[] args){
-        BinaryData test = new BinaryData(10);
-
         System.out.print("Binäre Zahl: ");
-        System.out.println(test.data);
     }
 }
