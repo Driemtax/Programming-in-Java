65b1d0508ef60fdaca39374c975a2e23586dd892
Driemtax <driemtax@gmail.com>
2024-10-15 10:08:03 +0200

    Added Main Function for Helper if needed solo instance

--- Math/Utility/Helper.java
+++ Math/Utility/Helper.java
@@ -1,6 +1,24 @@
 package Math.Utility;
 
 public class Helper {
+    // Uncomment to use CGD as single programm
+    /*public static void main(String[] args) {
+        while (true) {
+            System.out.println("\n");
+            System.out.println("Welcome to the jungle!");
+            System.out.println("Type in n: ");
+            int n = Integer.parseInt(System.console().readLine());
+            System.out.println("Type in m: ");
+            int m = Integer.parseInt(System.console().readLine());
+
+            int biggerNumber = n >= m ? n : m;
+            int smallerNumber = n < m ? n : m;
+            int result = CGD(biggerNumber, smallerNumber);
+
+            System.out.println("Result: " + result);
+        }
+    }*/
+
     /*Method to calculate the common greatest divisor of 2 numbers with euklidian algorithm
     Formula is m = q1 * n + b1 where m is the bigger number and n the smaller. 
     Final n is the gcd of m and n*/
